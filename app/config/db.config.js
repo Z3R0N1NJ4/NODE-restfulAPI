5d5a5eefdae76eca0d6345a28a76b003899aeb85
Mishaine <114750133+Z3R0N1NJ4@users.noreply.github.com>
2023-02-13 14:27:26 +0200

    Add files via upload

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "b3munkt2gz0n9s4aksww-mysql.services.clever-cloud.com",
+    USER: "u8bhi3anxv2apkzt",
+    PASSWORD: "QRtGovbcuOeMRkUPh9RW",
+    DB: "b3munkt2gz0n9s4aksww",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
